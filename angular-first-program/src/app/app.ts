b41bbdd682766a3667526d8d7654897b111d307c
gagan1146 <gagansharma99690@gmail.com>
2025-11-22 20:49:07 +0530

    First Change in the HTML and TS file for the properties

---
+++ angular-first-program/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('angular-first-program');
+  title1 = 'angular-first-program';
+  description = 'This is my first Angular program';
+}
